f4c65c247bec86ed1a030c95ef252baf116ec85f
jweismannCC <jonathan.weismann@clarecontrols.com>
2019-07-25 16:48:24 -0400

    Update main.go
    added special messages.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "I Know the Kubernetez"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
